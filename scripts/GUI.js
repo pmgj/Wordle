d94fffd05821690d24e7345e6e8d7f0a6c4f5d36
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2024-04-18 16:55:12 -0300

    Share code with Dordle

--- scripts/GUI.js
+++ scripts/GUI.js
@@ -5,14 +5,122 @@ import NotInWordListError from "./NotInWordListError.js";
 
 class GUI {
     constructor() {
-        this.currentRow = 0;
-        this.currentCol = 0;
-        this.currentWord = "";
-        this.wordle = new Wordle(words);
-        this.tbody = document.querySelector("tbody");
+        let tBodies = document.querySelectorAll("tbody");
+        this.wordle = [];
+        for (const table of tBodies) {
+            this.wordle.push({ game: new Wordle(words, 6), row: 0, col: 0, currentWord: "", tbody: table, isOver: false });
+        }
     }
-    keyPressed(evt) {
-        this.process(evt.key);
+    showWord(mr, tabindex) {
+        if (this.wordle[tabindex].isOver) return;
+        let endOfGame = () => {
+            this.wordle[tabindex].col = 0;
+            this.wordle[tabindex].currentWord = "";
+            if (this.wordle[tabindex].isOver) return;
+            this.wordle[tabindex].row++;
+            if (mr.winner === Winner.WIN) {
+                let j = 1, td = this.wordle[tabindex].tbody.rows[this.wordle[tabindex].row - 1].cells[0];
+                let endanim = cell => {
+                    cell.dataset.animation = "bounce";
+                    if (cell.nextSibling) {
+                        setTimeout(() => endanim(cell.nextSibling), j++ * 100);
+                    }
+                };
+                endanim(td);
+                this.wordle[tabindex].isOver = true;
+            }
+            let message = document.querySelector("#message");
+            if (this.wordle.every(w => w.isOver)) {
+                window.onkeyup = undefined;
+                message.textContent = "Congratulations!";
+                message.className = "bg-success text-white";
+            } else if (mr.winner === Winner.LOSE) {
+                window.onkeyup = undefined;
+                message.textContent = `You lose! ${this.wordle.map(mr => mr.game.secret.toUpperCase())}`;
+                message.className = "bg-secondary text-white";
+            }
+        };
+        let styleKeyboard = () => {
+            for (let i = 0; i < mr.hint.length; i++) {
+                let index = mr.hint[i];
+                let letter = this.wordle[tabindex].currentWord[i].toLowerCase();
+                let b = document.querySelector(`button[data-value='${letter}']`);
+                let bStyles = ["btn-secondary", "btn-warning", "btn-success"];
+                b.classList.remove("bg-secondary-subtle");
+                b.classList.add("text-white");
+                b.classList.add(bStyles[index]);
+            }
+            endOfGame();
+        };
+        let animation = cell => {
+            let bgStyles = ["bg-secondary", "bg-warning", "bg-success"];
+            let bdStyles = ["border-secondary", "border-warning", "border-success"];
+            cell.dataset.animation = "flip-in";
+            cell.onanimationend = () => {
+                cell.dataset.animation = "flip-out";
+                cell.classList.add("text-white");
+                cell.classList.add(bgStyles[mr.hint[cell.cellIndex]]);
+                cell.classList.add(bdStyles[mr.hint[cell.cellIndex]]);
+                cell.onanimationend = () => {
+                    cell.onanimationend = undefined;
+                    if (cell.nextSibling) {
+                        animation(cell.nextSibling);
+                    } else {
+                        styleKeyboard();
+                    }
+                };
+            };
+        };
+        let td = this.wordle[tabindex].tbody.rows[this.wordle[tabindex].row].cells[0];
+        animation(td);
+    }
+    checkWord() {
+        for (let i = 0; i < this.wordle.length; i++) {
+            try {
+                let temp = this.wordle[i].game.check(this.wordle[i].currentWord);
+                this.showWord(temp, i);
+            } catch (ex) {
+                if (ex instanceof NotInWordListError) {
+                    let tr = this.wordle[i].tbody.rows[this.wordle[i].row];
+                    for (let j = 0; j < tr.cells.length; j++) {
+                        tr.cells[j].dataset.animation = "shake";
+                        tr.cells[j].onanimationend = () => {
+                            tr.cells[j].dataset.animation = "";
+                        };
+                    }
+                }
+            }
+        }
+    }
+    removeLetter() {
+        for (let i = 0; i < this.wordle.length; i++) {
+            let temp = this.wordle[i];
+            if (temp.col === 0) {
+                continue;
+            }
+            temp.currentWord = temp.currentWord.slice(0, -1);
+            temp.col--;
+            if (!temp.isOver) {
+                let td = temp.tbody.rows[temp.row].cells[temp.col];
+                td.textContent = "";
+                td.dataset.animation = "";
+            }
+        }
+    }
+    addLetter(letter) {
+        for (let i = 0; i < this.wordle.length; i++) {
+            let temp = this.wordle[i];
+            if (temp.currentWord.length >= temp.game.wordLength) {
+                return;
+            }
+            if (!temp.isOver) {
+                let td = temp.tbody.rows[temp.row].cells[temp.col];
+                td.textContent = letter;
+                td.dataset.animation = "pop";
+                temp.currentWord += letter;
+                temp.col++;
+            }
+        }
     }
     process(key) {
         switch (key) {
@@ -27,120 +135,28 @@ class GUI {
                     this.addLetter(key);
         }
     }
-    checkWord() {
-        let mr;
-        try {
-            mr = this.wordle.check(this.currentWord);
-            this.showWord(mr);
-        } catch (ex) {
-            if (ex instanceof NotInWordListError) {
-                let tr = this.tbody.rows[this.currentRow];
-                for (let i = 0; i < tr.cells.length; i++) {
-                    tr.cells[i].dataset.animation = "shake";
-                    tr.cells[i].onanimationend = () => {
-                        tr.cells[i].dataset.animation = "";
-                    };
-                }
-            }
-        }
-    }
-    showWord(mr) {
-        let bgStyles = ["bg-secondary", "bg-warning", "bg-success"];
-        let bdStyles = ["border-secondary", "border-warning", "border-success"];
-        let endOfGame = () => {
-            this.currentRow++;
-            this.currentCol = 0;
-            this.currentWord = "";
-            let message = document.querySelector("#message");
-            switch (mr.winner) {
-                case Winner.WIN:
-                    window.onkeyup = undefined;
-                    message.textContent = "Congratulations!";
-                    message.className = "bg-success text-white";
-                    message.style.visibility = "visible";
-                    let i = 1, td = this.tbody.rows[this.currentRow - 1].cells[0];
-                    let endanim = cell => {
-                        cell.dataset.animation = "bounce";
-                        if (cell.nextSibling) {
-                            setTimeout(() => endanim(cell.nextSibling), i++ * 100);
-                        }
-                    };
-                    endanim(td);
-                    break;
-                case Winner.LOSE:
-                    window.onkeyup = undefined;
-                    message.textContent = `You lose! Correct word: ${mr.code.toUpperCase()}`;
-                    message.className = "bg-secondary text-white";
-                    message.style.visibility = "visible";
-                    break;
-            }
-        };
-        let styleKeyboard = () => {
-            for (let i = 0; i < mr.hint.length; i++) {
-                let index = mr.hint[i];
-                let letter = this.currentWord[i].toLowerCase();
-                let b = document.querySelector(`button[data-value='${letter}']`);
-                b.classList.remove("bg-secondary-subtle");
-                b.classList.remove("btn-warning");
-                let bStyles = ["btn-secondary", "btn-warning", "btn-success"];
-                b.classList.add(bStyles[index]);
-                let td = this.tbody.rows[this.currentRow].cells[i];
-                td.onanimationend = undefined;
-            }
-            endOfGame();
-        };
-        let animation = cell => {
-            cell.dataset.animation = "flip-in";
-            cell.onanimationend = () => {
-                cell.dataset.animation = "flip-out";
-                cell.classList.add("text-white");
-                cell.classList.add(bgStyles[mr.hint[cell.cellIndex]]);
-                cell.classList.add(bdStyles[mr.hint[cell.cellIndex]]);
-                cell.onanimationend = () => {
-                    if (cell.nextSibling) {
-                        animation(cell.nextSibling);
-                    } else {
-                        styleKeyboard();
-                    }
-                };
-            };
-        };
-        let td = this.tbody.rows[this.currentRow].cells[0];
-        animation(td);
-    }
-    removeLetter() {
-        if (this.currentCol === 0) {
-            return;
-        }
-        this.currentWord = this.currentWord.slice(0, -1);
-        this.currentCol--;
-        let td = this.tbody.rows[this.currentRow].cells[this.currentCol];
-        td.textContent = "";
-        td.dataset.animation = "";
-    }
-    addLetter(letter) {
-        if (this.currentWord.length >= this.wordle.wordLength) {
-            return;
-        }
-        this.currentWord += letter;
-        let td = this.tbody.rows[this.currentRow].cells[this.currentCol];
-        td.textContent = letter;
-        td.dataset.animation = "pop";
-        this.currentCol++;
+    keyPressed(evt) {
+        this.process(evt.key);
     }
     buttonPressed(evt) {
         this.process(evt.currentTarget.dataset.value);
     }
-    registerEvents() {
-        let rows = "";
-        for (let i = 0; i < this.wordle.maxTries; i++) {
-            rows += "<tr>";
-            for (let j = 0; j < this.wordle.wordLength; j++) {
-                rows += "<td></td>";
+    fillBoard() {
+        for (let i = 0; i < this.wordle.length; i++) {
+            const element = this.wordle[i];
+            let rows = "";
+            for (let i = 0; i < element.game.maxTries; i++) {
+                rows += "<tr>";
+                for (let j = 0; j < element.game.wordLength; j++) {
+                    rows += "<td></td>";
+                }
+                rows += "</tr>";
             }
-            rows += "</tr>";
+            element.tbody.innerHTML = rows;
         }
-        this.tbody.innerHTML = rows;
+    }
+    registerEvents() {
+        this.fillBoard();
         window.onkeyup = this.keyPressed.bind(this);
         let buttons = document.querySelectorAll("button");
         buttons.forEach(b => b.onclick = this.buttonPressed.bind(this));
--- scripts/Wordle.js
+++ scripts/Wordle.js
@@ -3,11 +3,11 @@ import MoveResult from "./MoveResult.js";
 import NotInWordListError from "./NotInWordListError.js";
 
 export default class Wordle {
-    constructor(words) {
+    constructor(words, tries) {
         this.words = words;
         this.secret = words[Math.floor(Math.random() * words.length)];
         this.tries = 0;
-        this.maxTries = 6;
+        this.maxTries = tries;
         this.wordLength = this.secret.length;
     }
     check(value) {
